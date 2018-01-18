d14ea8a523f4264c2a806a837e45586e540020b6
Patrick <kaw.patrick@yahoo.de>
2018-01-18 11:56:43 +0100

    set static positions for first and last node

--- src/de/bytedev/exercise9/FreeBoundsOscillators.java
+++ src/de/bytedev/exercise9/FreeBoundsOscillators.java
@@ -4,19 +4,25 @@ public class FreeBoundsOscillators extends AbstractOscillators {
 
     public FreeBoundsOscillators(int mode, int N, double kValues, boolean randU) {
         super(mode, N, kValues, randU);
+
+        this.getState()[0] = this.getState()[2];
+        this.getState()[ this.getState().length-3 ] = this.getState()[ this.getState().length - 5 ];
     }
 
     @Override
     public void getRate(double[] state, double[] rate) {
+
+        state[0] = state[2];
+        state[ state.length-3 ] = state[ state.length - 5 ];
 
         for(int i=0; i < this.getState().length - 2; i += 2 ) {
             // nodes change their displacement by their velocity
             rate[i] = state[i+1];
 
             if(i == 0) {
-                rate[i] = rate[i+2];
+                rate[i] = state[i+3];
             } else if(i == this.getState().length - 3) {
-                rate[i] = rate[i-2];
+                rate[i] = state[i-1];
             }
 
             if(i == 0 || i >= this.getState().length-3) {
